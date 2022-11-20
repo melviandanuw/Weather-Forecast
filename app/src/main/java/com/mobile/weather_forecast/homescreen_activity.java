3e83c4ea784e56a5e94a211f112d0a0c25d2f355
Melvian Danu Wijaya <melvian.danuw@gmail.com>
2022-11-20 11:48:06 +0700

    Initial commit

---
+++ app/src/main/java/com/mobile/weather_forecast/homescreen_activity.java
@@ -0,0 +1,53 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		homescreen
+	 *	@date 		Saturday 12th of November 2022 08:30:08 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.mobile.weather_forecast;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class homescreen_activity extends Activity {
+
+	
+	private View _bg__homescreen_ek2;
+	private ImageView aesthetic_fall_autumn_phone_wallpaper_backgrounds_android_ios_17_1;
+	private View background;
+	private ImageView vector;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.homescreen);
+
+		
+		_bg__homescreen_ek2 = (View) findViewById(R.id._bg__homescreen_ek2);
+		aesthetic_fall_autumn_phone_wallpaper_backgrounds_android_ios_17_1 = (ImageView) findViewById(R.id.aesthetic_fall_autumn_phone_wallpaper_backgrounds_android_ios_17_1);
+		background = (View) findViewById(R.id.background);
+		vector = (ImageView) findViewById(R.id.vector);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
---
+++ app/src/main/java/com/mobile/weather_forecast/mainmenu_activity.java
@@ -0,0 +1,61 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc
+	 *	@file 		mainmenu
+	 *	@date 		Saturday 12th of November 2022 08:31:54 AM
+	 *	@title 		Page 1
+	 *	@author
+	 *	@keywords
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+
+
+package com.mobile.weather_forecast;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class mainmenu_activity extends Activity {
+
+
+	private View _bg__mainmenu_ek2;
+	private ImageView x00l83va93u31_1;
+	private View background;
+	private ImageView shape_1;
+	private ImageView weather_big;
+	private ImageView weather_small;
+	private View rectangle_8;
+	private ImageView menu;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainmenu);
+
+
+		_bg__mainmenu_ek2 = (View) findViewById(R.id._bg__mainmenu_ek2);
+		x00l83va93u31_1 = (ImageView) findViewById(R.id.x00l83va93u31_1);
+		background = (View) findViewById(R.id.background);
+		shape_1 = (ImageView) findViewById(R.id.shape_1);
+		weather_big = (ImageView) findViewById(R.id.weather_big);
+		weather_small = (ImageView) findViewById(R.id.weather_small);
+		rectangle_8 = (View) findViewById(R.id.rectangle_8);
+		menu = (ImageView) findViewById(R.id.menu);
+
+
+		//custom code goes here
+	
+	}
+}
+	
+	
